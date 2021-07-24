73741b6583dd0502bf309ddbdf20be38467f503f
Jack Yu <455501914@qq.com>
2021-07-24 14:48:52 +0800

    Update collector.py

--- collector.py
+++ collector.py
@@ -74,7 +74,7 @@ def main(time_flag):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG,
+    logging.basicConfig(level=logging.WARNING,
                         format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                         datefmt='%a, %d %b %Y %H:%M:%S',
                         filename='collector.log',
